d36f4c1743cd4778b869e2b7b3cea5041597660a
Sudhanshu Ranjan <sudhanshu99964@gmail com>
2026-09-13 15:43:47 +0530

    updated java program

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,5 +1,12 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("First number = " + a);
+        System.out.println("Second number = " + b);
+        System.out.println("Sum = " + sum);
     }
 }
